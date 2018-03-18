dd3e5b77f9ef11b68e46851329e9edfa0fa6ef9f
howdy39 <tatsuya.howdy@gmail.com>
2018-03-18 16:54:22 +0900

    Chore: add deploy command and fix indents

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require('path');
 const GasPlugin = require("gas-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.ts',
-    devtool: 'production',
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader'
-            }
-        ]
-    },
-    resolve: {
-        extensions: [
-            '.ts'
-        ]
-    },
-    plugins: [
-        new GasPlugin()
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'production',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
     ]
+  },
+  resolve: {
+    extensions: [
+      '.ts'
+    ]
+  },
+  plugins: [
+    new GasPlugin()
+  ]
 };
